c2cb445b0cf66f464d67588de4d2ba0cae6e5166
arifeen008 <feenkundee@gmail.com>
2025-07-25 10:58:10 +0700

    ok

--- resources/views/office/credits/uploadcredit.blade.php
+++ resources/views/office/credits/uploadcredit.blade.php
@@ -16,7 +16,7 @@
                     <div>
                         <label for="memberID" class="block text-sm font-medium text-gray-700">เลขที่สมาชิก <span
                                 class="text-red-500">*</span></label>
-                        <input type="text" id="memberID" name="memberID"
+                        <input type="text" id="memberID" name="memberID" maxlength="5"
                             class="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md focus:ring-green-500 focus:border-green-500"
                             placeholder="เลขที่สมาชิก" required>
                     </div>
